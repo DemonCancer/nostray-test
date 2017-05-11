25b2493b242bfd3eb89bec3c09eaa8163b0f70fe
Klawens <lscllyx@icloud.com>
2017-05-11 17:54:53 +0800

    App changes

--- nostray/urls.py
+++ nostray/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls'))
+    url(r'^raise/', include('raise.urls'))
+    url(r'^register/$', include('register.urls'))
 ]
--- raise/urls.py
+++ raise/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
-    url(r'^hello/', views.index)
+    url(r'^stations/$', views.)
+    url(r'^gift/$', view.)
 ]
