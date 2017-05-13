e830ea06d9a7a333ebc83af843ca2f9f74df9a8d
Klawens <lscllyx@icloud.com>
2017-05-13 13:51:23 +0800

    Db

--- raise/models.py
+++ raise/models.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class station_name(models.Model):
+	img = models.TextField(default='./pics/empty_img')
+	name = models.CharField(max_length=32, default="NULL")
