7f5a46c9036d9377f4d36c846e076ea30e51ad1c
Klawens <lscllyx@icloud.com>
2017-05-21 19:45:27 +0800

    still some problems

--- nostray/settings.py
+++ nostray/settings.py
@@ -120,15 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-SITE_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), '')
-
-STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
-
 STATIC_URL = '/static/'
-
-STATICFILES_DIR = [
-    os.path.join(STATIC_ROOT, 'css'),
-    os.path.join(STATIC_ROOT, 'js'),
-    os.path.join(STATIC_ROOT, 'images'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static/"),
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
