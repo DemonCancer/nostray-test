69561bac54f9f96cebc856b566db5e0cbc492908
Klawens <lscllyx@icloud.com>
2017-05-29 20:14:39 +0800

    sqlite3->mysql

--- nostray/settings.py
+++ nostray/settings.py
@@ -75,14 +75,25 @@ WSGI_APPLICATION = 'nostray.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
+'''
+#sqlite
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nostray',
+        'USER': 'root',
+        'PASSWORD': '0609',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
